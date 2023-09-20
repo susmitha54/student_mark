3b68a63a8debf2ed530dca90bbfa226fabe15246
susmitha54 <143336106+susmitha54@users.noreply.github.com>
2023-09-20 14:07:51 +0530

    Add files via upload

---
+++ subject.py
@@ -0,0 +1,16 @@
+subject=[]
+marks=[] 
+sum=0
+x=int(input("Enter no.of subject: "))
+for i in range(x):
+     subject.append(x)
+  
+for i in range(x):
+    y=int(input("Enter marks of the subject:"))
+    marks.append(y) 
+    sum=sum+y             
+    if y>35:
+        print(input("PASS"))
+    else:
+        print(input("fail"))     
+print("Sum of mark is:",sum)        
